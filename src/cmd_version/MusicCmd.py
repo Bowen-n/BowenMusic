576f6de1fc848b38a2f74c2b5286072dccb1837e
Bowen <bowenduan@users.noreply.github.com>
2020-03-31 21:21:59 +0800

    Add search

--- src/cmd_version/MusicCmd.py
+++ src/cmd_version/MusicCmd.py
@@ -4,11 +4,9 @@ import sys
 from cmd import Cmd
 
 import mpv
-
+from wcwidth import wcswidth
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from api import MiguMusicAPI, NeteaseCloudMusicAPI, QQMusicApi
-
-
+from api import MiguMusicAPI, NeteaseCloudMusicAPI, QQMusicApi, convert_interval
 
 
 class MusicShell(Cmd):
@@ -21,6 +19,7 @@ class MusicShell(Cmd):
         Cmd.__init__(self)
         self.playlist = list()
         self._get_playlist_info()
+        self.api = NeteaseCloudMusicAPI()
 
 
     def _get_playlist_info(self):
@@ -58,12 +57,55 @@ class MusicShell(Cmd):
                 for index in range(len(song_list)):
                     print('{:2}. {:\u3000<12}{:\u3000<10}{:<}'.format(index+1, 
                         song_list[index]['song_name'], song_list[index]['singers'], song_list[index]['album_name']))
-
+                print('-'*70)
             except:
                 print('Args error.')
 
+
     def do_search(self, keyword):
-        pass
+        if self.api.name == 'qq':
+            self.search_result = self.api.search(1, keyword)
+            self._show_search()
+        elif self.api.name == 'netease':
+            self.search_result = self.api.search(keyword, 1)
+            self._show_search()
+        elif self.api.name == 'migu':
+            self.search_result = self.api.search(keyword)
+            self._show_search()
+        else:
+            print('api name not found.')
+            return
+
+
+    def _show_search(self):
+        print('{}{}{}{}'.format(
+            self._rpad('歌曲', 40), 
+            self._rpad('歌手', 30), 
+            self._rpad('专辑', 40), 
+            self._rpad('时长', 40)))
+        print('-'*120)
+        index = 1
+        for result in self.search_result:
+            singer_list = result['singer_list']
+            singers = ''
+            for singer in singer_list:
+                singers += singer+'/'
+            singers = singers.strip('/')
+            print('{}. {}{}{}{}'.format(
+                index,
+                self._rpad(result['song_name'], 40), 
+                self._rpad(singers, 30),
+                self._rpad(result['album_name'], 40), 
+                self._rpad(convert_interval(result['interval']), 40)))
+            index += 1
+            if index == 11:
+                break
+        print('-'*120)
+
+    def _lpad(self, text, length, padding=' '):
+        return padding * max(0, (length-wcswidth(text))) + text
+    def _rpad(self, text, length, padding=' '):
+        return text + padding * max(0, (length-wcswidth(text)))
 
     def do_help(self, arg):
         pass
